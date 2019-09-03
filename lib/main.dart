a269177dee1b51802df75a7b39ae0b379ffdda54
zegkljan <zegkljan@gmail.com>
2019-09-04 00:45:51 +0200

    Colors, drawer, bottom menu.
    * Set up a basic color theme.
    * Added a side drawer with a few settings with mock behaviour.
    * Test-added QR code scanner for pairing with server.
    * Zoom and to-current-position buttons moved to bottom bar.
    * Added numerical indication of Lat/Lng.

--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,40 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:typed_data';
+import 'dart:developer' as developer;
 
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:latlong/latlong.dart';
 import 'package:location/location.dart';
-import 'dart:developer' as developer;
-
-import 'package:poi_map_app/crosshair_plugin.dart';
 
 void main() => runApp(MaterialApp(
-  title: 'Černobor POI mapa',
-  theme: ThemeData(
-    primaryColor: Colors.lightGreen,
-  ),
-  home: Map(),
-));
+      title: 'Černobor POI mapa',
+      theme: ThemeData(
+        primaryColor: Color(0xFF33691e),
+        primaryColorBrightness: Brightness.dark,
+        primaryColorDark: Color(0xFF003d00),
+        primaryColorLight: Color(0xFF629749),
+        accentColor: Color(0xFF827717),
+        accentColorBrightness: Brightness.dark,
+      ),
+      home: Map(),
+    ));
 
 class MapState extends State<Map> {
   final MapController mapController = MapControllerImpl();
   final Location location = Location();
   final double maxZoom = 20;
-  final double minZoom = 7;
+  final double minZoom = 1;
   bool locationEnabled = false;
   LatLng currentLocation;
+
+  bool pairing = false;
+  bool paired = false;
+  bool pinging = false;
+  bool serverAvailable = false;
 
   @override
   void initState() {
@@ -45,13 +53,41 @@ class MapState extends State<Map> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      primary: true,
       appBar: AppBar(
-        /*leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Menu',
-          onPressed: null,
-        ),*/
-        title: Text('TODO title')
+        title: Text('Černobor location mapper'),
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text('Paired to server'),
+              trailing: pairing
+                  ? Container(
+                      child: CircularProgressIndicator(
+                          value: null, strokeWidth: 2.5),
+                      height: 16,
+                      width: 16,
+                    )
+                  : Icon(paired ? Icons.done : Icons.clear),
+              onTap: this.onPair,
+            ),
+            ListTile(
+              title: Text('Server available'),
+              enabled: paired,
+              trailing: paired && pinging
+                  ? Container(
+                      child: CircularProgressIndicator(
+                          value: null, strokeWidth: 2.5),
+                      height: 16,
+                      width: 16,
+                    )
+                  : Icon(paired && serverAvailable ? Icons.done : Icons.clear),
+              onTap: this.onPing,
+            ),
+          ],
+        ),
       ),
       body: FlutterMap(
         options: MapOptions(
@@ -60,90 +96,127 @@ class MapState extends State<Map> {
           maxZoom: maxZoom,
           minZoom: minZoom,
           debug: true,
-          onPositionChanged: !mapController.ready ? null : (MapPosition position, bool hasGesture) {
-            setState(() {});
-          },
+          onPositionChanged: !mapController.ready
+              ? null
+              : (MapPosition position, bool hasGesture) {
+                  setState(() {});
+                },
         ),
         layers: [
           TileLayerOptions(
-            urlTemplate: 'http://192.168.1.2:8080/styles/{id}/{z}/{x}/{y}@2x.png',
-            additionalOptions: {
-              'id': 'osm-bright'
-            },
+            urlTemplate: 'http://10.0.2.2:8080/styles/{id}/{z}/{x}/{y}@2x.png',
+            additionalOptions: {'id': 'osm-bright'},
           ),
           MarkerLayerOptions(
-            markers: currentLocation == null ? [] : [
-              Marker(
-                width: 30,
-                height: 30,
-                anchorPos: AnchorPos.align(AnchorAlign.center),
-                point: currentLocation,
-                builder: (context) {
-                  return Icon(Icons.my_location,
-                    color: Theme.of(context).accentColor,
-                  );
-                }
-              )
-            ]
-          ),
+              markers: currentLocation == null
+                  ? []
+                  : [
+                      Marker(
+                          width: 30,
+                          height: 30,
+                          anchorPos: AnchorPos.align(AnchorAlign.center),
+                          point: currentLocation,
+                          builder: (context) {
+                            return Icon(
+                              Icons.my_location,
+                              color: Theme.of(context).accentColor,
+                            );
+                          })
+                    ]),
           MarkerLayerOptions(
-            markers: mapController.ready ? [
-              Marker(
-                width: 100,
-                height: 100,
-                anchorPos: AnchorPos.align(AnchorAlign.center),
-                point: mapController.center,
-                builder: (context) {
-                  return Icon(
-                    Icons.add,
-                    size: 100,
-                    color: Color.fromRGBO(0, 0, 0, 0.5),
-                  );
-                }
-              )
-            ] : []
-          )
+              markers: mapController.ready
+                  ? [
+                      Marker(
+                          width: 100,
+                          height: 100,
+                          anchorPos: AnchorPos.align(AnchorAlign.center),
+                          point: mapController.center,
+                          builder: (context) {
+                            return Icon(
+                              Icons.add,
+                              size: 100,
+                              color: Color.fromRGBO(0, 0, 0, 0.5),
+                            );
+                          })
+                    ]
+                  : [])
         ],
         mapController: mapController,
       ),
-      floatingActionButton: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(5),
-            child: FloatingActionButton(
-              tooltip: 'Zoom in',
-              child: Icon(Icons.zoom_in),
-              backgroundColor: mapController.ready && mapController.zoom < maxZoom ? Theme.of(context).accentColor : Theme.of(context).disabledColor,
-              onPressed: () {
-                mapController.move(mapController.center, mapController.zoom + 1);
-              },
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                      'Lat: ${mapController.ready ? mapController.center.latitude.toStringAsPrecision(8) : '-'}',
+                      style: Theme.of(context)
+                          .primaryTextTheme
+                          .body1
+                          .apply(fontFamily: 'monospace')),
+                  Text(
+                      'Lng: ${mapController.ready ? mapController.center.longitude.toStringAsPrecision(8) : '-'}',
+                      style: Theme.of(context)
+                          .primaryTextTheme
+                          .body1
+                          .apply(fontFamily: 'monospace')),
+                ],
+              )
             ),
-          ),
-          Container(
-            padding: EdgeInsets.all(5),
-            child: FloatingActionButton(
-              tooltip: 'Zoom out',
-              child: Icon(Icons.zoom_out),
-              backgroundColor: mapController.ready && mapController.zoom > minZoom ? Theme.of(context).accentColor : Theme.of(context).disabledColor,
-              onPressed: () {
-                mapController.move(mapController.center, mapController.zoom - 1);
-              },
+            Expanded(
+              flex: 0,
+              child: IconButton(
+                tooltip: 'Acquire and center on current position',
+                icon: Icon(
+                    mapController.ready && currentLocation == mapController.center
+                        ? Icons.my_location
+                        : Icons.location_searching),
+                iconSize: 30.0,
+                color: locationEnabled
+                    ? Theme.of(context).colorScheme.onPrimary
+                    : Theme.of(context).disabledColor,
+                onPressed: this.setLocation,
+              ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.all(5),
-            child: FloatingActionButton(
-              tooltip: 'Get current position',
-              child: Icon(mapController.ready && currentLocation == mapController.center ? Icons.my_location : Icons.location_searching),
-              backgroundColor: locationEnabled ? Colors.red : Colors.black,
-              onPressed: this.setLocation,
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  IconButton(
+                    tooltip: 'Zoom in',
+                    icon: Icon(Icons.zoom_in),
+                    iconSize: 30.0,
+                    color: mapController.ready && mapController.zoom < maxZoom
+                        ? Theme.of(context).colorScheme.onPrimary
+                        : Theme.of(context).disabledColor,
+                    onPressed: () {
+                      mapController.move(
+                          mapController.center, mapController.zoom + 1);
+                    },
+                  ),
+                  IconButton(
+                    tooltip: 'Zoom out',
+                    icon: Icon(Icons.zoom_out),
+                    iconSize: 30.0,
+                    color: mapController.ready && mapController.zoom > minZoom
+                        ? Theme.of(context).colorScheme.onPrimary
+                        : Theme.of(context).disabledColor,
+                    onPressed: () {
+                      mapController.move(
+                          mapController.center, mapController.zoom - 1);
+                    },
+                  ),
+                ]
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
+        color: Theme.of(context).primaryColor,
       ),
-      //floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
     );
   }
 
@@ -153,6 +226,55 @@ class MapState extends State<Map> {
         developer.log('$currentLocation');
         currentLocation = LatLng(loc.latitude, loc.longitude);
         mapController.move(currentLocation, mapController.zoom);
+      });
+    });
+  }
+
+  void onPair() {
+    FlutterBarcodeScanner.scanBarcode("#ff6666", "Cancel", false).then((String value) {
+      developer.log('Scanned code: $value');
+      setState(() {
+        pairing = true;
+        Future.delayed(Duration(seconds: 1)).then((_) {
+          setState(() {
+            pairing = false;
+            paired = true;
+          });
+          Future.delayed(Duration(seconds: 1)).then((_) {
+            setState(() {
+              pairing = true;
+              Future.delayed(Duration(seconds: 1)).then((_) {
+                setState(() {
+                  pairing = false;
+                  paired = false;
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  }
+
+  void onPing() {
+    setState(() {
+      pinging = true;
+      Future.delayed(Duration(seconds: 1)).then((_) {
+        setState(() {
+          pinging = false;
+          serverAvailable = true;
+        });
+        Future.delayed(Duration(seconds: 1)).then((_) {
+          setState(() {
+            pinging = true;
+            Future.delayed(Duration(seconds: 1)).then((_) {
+              setState(() {
+                pinging = false;
+                serverAvailable = false;
+              });
+            });
+          });
+        });
       });
     });
   }
